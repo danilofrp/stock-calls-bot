3e5fa201b61a90c23ce83452172fe36f55a08325
Danilo Filippo <danilo.frp@poli.ufrj.br>
2020-02-03 19:23:55 -0300

    Changes parameter name for consistency

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import json
 
-def read_json(file):
-    with open(file, 'r') as f:
+def read_json(filepath):
+    with open(filepath, 'r') as f:
         json_str = f.read()
         data = json.loads(json_str)
     return data
@@ -13,8 +13,8 @@ def read_json_from_s3(s3_client, bucket, filepath):
     return data
 
 
-def write_json(file, data):
-    with open(file, 'w') as f:
+def write_json(filepath, data):
+    with open(filepath, 'w') as f:
         json_str = json.dumps(data)
         f.write(json_str)
         
