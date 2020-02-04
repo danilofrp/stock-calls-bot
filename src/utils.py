539e639398e2bf98ca7969dbbfdafcd62e3e30ab
Danilo Filippo <danilo.frp@poli.ufrj.br>
2020-02-03 21:23:38 -0300

    Adds function to check if file exists in S3

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,20 @@
 import json
+import botocore
 
-def read_json(filepath):
-    with open(filepath, 'r') as f:
+
+def exists_in_s3(s3_client, bucket, filepath, logger):
+    try:
+        s3_client.Object(bucket, filepath).load()
+        return True
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return False
+        else:
+            raise e
+
+
+def read_json(file):
+    with open(file, 'r') as f:
         json_str = f.read()
         data = json.loads(json_str)
     return data
@@ -13,11 +26,12 @@ def read_json_from_s3(s3_client, bucket, filepath):
     return data
 
 
-def write_json(filepath, data):
-    with open(filepath, 'w') as f:
+def write_json(file, data):
+    with open(file, 'w') as f:
         json_str = json.dumps(data)
         f.write(json_str)
         
         
 def write_json_to_s3(s3_client, bucket, filepath, data):
+    # s3.Bucket(bucket).put_object(Key=filepath, Body=json.dumps(data))
     s3_client.Object(bucket, filepath).put(Body=json.dumps(data))
